478d826e48d9388ee8e73e83aa96a2e285e3d9c4
Ashley Sharpe <ashleysharpe00@gmail.com>
2021-01-29 23:42:52 -0600

    End of 4.12 Delete , Update added

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -56,5 +56,15 @@ namespace ElevenNote.WebAPI.Controllers
                 return InternalServerError();
             return Ok();
         }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
